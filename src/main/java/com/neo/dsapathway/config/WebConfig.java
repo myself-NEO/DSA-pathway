ffa07096a319f85a2afba05ca8faf33afeea59dc
Keshav Kumar Jha <40928377+myself-NEO@users.noreply.github.com>
2024-10-07 02:57:21 +0530

    added cors config

---
+++ src/main/java/com/neo/dsapathway/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.neo.dsapathway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow all paths
+                .allowedOrigins("https://keshav-jha.netlify.app")  // Allow these origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow these HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow credentials like cookies
+    }
+}
